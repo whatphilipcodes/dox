8a5b9a6e319ada09c093facc408f233f1085df04
whatphilipcodes <gerdes.philip@gmail.com>
2025-01-05 13:48:02 +0100

    add: lucide-react

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_URL = 'https://dox.where';
 export const SITE_TITLE = 'dox';
 export const SITE_DESCRIPTION = 'documentation websites made simple';
+export const SITE_AUTHOR = 'whatphilipdoes';
