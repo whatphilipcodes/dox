d001a3562ad28eeb5d8708023a2d9116c256c630
whatphilipcodes <gerdes.philip@gmail.com>
2025-01-06 18:04:36 +0100

    add: geist font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-	darkMode: 'selector',
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Geist'],
+        mono: ['GeistMono'],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'selector',
+};
