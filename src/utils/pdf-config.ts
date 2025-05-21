cc4f091b260a96b512923e33064bd55df5f75877
whatphilipcodes <gerdes.philip@gmail.com>
2025-05-21 22:27:26 +0200

    fx: pagination

--- src/utils/pdf-config.ts
+++ src/utils/pdf-config.ts
@@ -1,10 +1,14 @@
 import type { Options, PageOptions } from 'astro-pdf';
 
+const scale = 1.3333333;
+// puppeteer uses 96 dpi instead of 72 to rasterize the pdf, to get the exact resolution in the output we need to multiply with 96 / 72
+
 const pageConfig: PageOptions = {
 	pdf: {
-		width: 1200,
-		height: 720,
+		width: 1920 * scale,
+		height: 1080 * scale,
 		printBackground: true,
+		displayHeaderFooter: false,
 	},
 	path: 'download/pdf/test.pdf',
 	waitUntil: 'networkidle0',
