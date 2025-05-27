c4a00701b811078b4d2b94225e1654135e79aa57
whatphilipcodes <gerdes.philip@gmail.com>
2025-05-27 03:02:58 +0200

    msc: move style to pdf

--- src/components/react/Dex/Slide.tsx
+++ src/components/react/Dex/Slide.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
 
 type ViewerProps = {
+	layout: 'default';
 	children: React.ReactNode;
 };
 const Slide: FC<ViewerProps> = ({ children }) => {
 	return (
-		<div className="aspect-video bg-secondary-500 break-after-page w-full h-full last:break-after-auto overflow-clip">
+		<div className="aspect-video bg-neutral-600 break-after-page w-full last:break-after-auto overflow-hidden p-4">
 			{children}
 		</div>
 	);
--- src/utils/pdf-config.ts
+++ src/utils/pdf-config.ts
@@ -1,10 +1,11 @@
-import { join, basename } from 'node:path';
+import { join, basename, parse } from 'node:path';
 import type { Options, PageOptions } from 'astro-pdf';
 import { ExifTool } from 'exiftool-vendored';
 import { SITE_AUTHOR } from '../consts';
 
 const scale = 1.3333333;
-// puppeteer uses 96 dpi instead of 72 to rasterize the pdf, to get the exact resolution in the output we need to multiply with 96 / 7
+const zoom = 1.2;
+// puppeteer uses 96 dpi instead of 72 to rasterize the pdf, to get the exact resolution in the output we need to multiply with 96 / 72
 const pageConfig = (width: number, height: number) => {
 	return {
 		pdf: {
@@ -32,23 +33,46 @@ const pdfConfig = (
 				return {
 					...pageConfig(width, height),
 					path: join(outDir, `${filename}.pdf`),
+					callback: async (page) => {
+						await page.evaluate((zoomValue: number) => {
+							const style = document.createElement('style');
+							style.innerHTML = `
+							  @media print {
+								  body * {
+									  zoom: ${zoomValue};
+									  visibility: hidden !important;
+								  }
+								  .pdf-export,
+								  .pdf-export * {
+									  visibility: visible !important;
+								  }
+							  }
+							`;
+							document.head.appendChild(style);
+						}, zoom);
+					},
 				};
 			}
 		},
 		runAfter: async (dir, pathnames) => {
-			const tool = new ExifTool({
-				taskTimeoutMillis: 5000,
-				forceWrite: true,
-				writeArgs: ['-all=', '-overwrite_original', `-Author=${SITE_AUTHOR}`],
-			});
 			for (const path of pathnames) {
+				const tool = new ExifTool({
+					taskTimeoutMillis: 5000,
+					forceWrite: true,
+					writeArgs: [
+						'-all=',
+						'-overwrite_original',
+						`-Title=${parse(path).name}`,
+						`-Author=${SITE_AUTHOR}`,
+					],
+				});
 				const abs = join(dir.pathname, path);
 				console.log('updating pdf metadata:', abs);
 				await tool.write(abs, {}).catch((e) => {
 					console.log(e);
 				});
+				tool.end();
 			}
-			tool.end();
 		},
 	} as Options;
 };
